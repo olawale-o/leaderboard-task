7485c56846373ed4f04fe551334edcce855b3ea2
olawale-o <omoogunolawale@gmail.com>
2021-07-26 21:39:32 +0100

    Create score-panel.js score-form.js and create-score.js files

---
+++ src/score-panel.js
@@ -0,0 +1,11 @@
+const createScorePanelHeader = () => {
+  const header = document.getElementById('score-panel-header');
+  const h2 = document.createElement('h2');
+  h2.textContent = 'Recent scores';
+  const button = document.createElement('button');
+  button.setAttribute('class', 'btn');
+  button.setAttribute('id', 'refresh-score');
+  header.append(h2, button);
+};
+
+export default createScorePanelHeader;
